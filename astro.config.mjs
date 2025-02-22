c8d72a9448c5f4a0ce4044c3056dc0d2fcebe8f4
Papershine <hilarylau119@gmail.com>
2025-02-22 17:41:01 -0500

    setup netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
 });
