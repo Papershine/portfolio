8d1f5e7422f3f15eecfd860aca91ea15d79d1a97
Papershine <hilarylau119@gmail.com>
2025-03-16 11:09:22 -0400

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
 
 import netlify from "@astrojs/netlify";
+
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: "server",
   adapter: netlify(),
-  integrations: [
-    partytown({
-        config: {
-          forward: ["dataLayer.push"],
-        },
-    }),
-],
+  integrations: [partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })],
 });
