6ec05c77a0119fa17f8fa885da58a4e85ce46c10
Papershine <hilarylau119@gmail.com>
2025-08-27 22:00:50 -0400

    Update design

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,8 @@ export default defineConfig({
     },
   },
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: "compile"
+  }),
   integrations: [react()],
 });
